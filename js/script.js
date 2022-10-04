cd1eb68f2f1167384e1c02c161b4e3c3c3991578
Ali Bahadoran <71150321+parsfront@users.noreply.github.com>
2022-10-04 05:21:22 +0000

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const colorBtns = document.querySelectorAll(".btn");
+
+colorBtns.forEach(function (colorBtn) {
+    colorBtn.addEventListener("click", function (event) {
+        let btnColor = event.target.dataset.color;
+
+        document.documentElement.style.setProperty("--theme-color", btnColor);
+    });
+});
